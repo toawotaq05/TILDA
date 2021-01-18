8701dc0a1e3758fda18292ad5f014d63b8d58ee2
toawotaq05 <cr0ntoz@gmail.com>
2021-01-18 12:22:16 +0100

    första

---
+++ TILDA/testFörsök.py
@@ -0,0 +1,11 @@
+print("verkar fungera")
+
+print("HELLO WORLD")
+2+4
+
+user.email = 
+	user.name = toawotaq05
+ 
+   git config --global user.email "cr0ntoz@gmail.com"
+   git config --global user.name "toawotaq05"
+   
